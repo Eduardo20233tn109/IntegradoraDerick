e76fa33b3468594ffb3e17929f5ef161c9eb7158
Eduardo Juárez <20233tn109@utez.edu.mx>
2024-12-02 09:40:57 -0600

    correccion enumerador personas

--- Item.java
+++ Item.java
@@ -13,7 +13,27 @@ public enum Item {
     ITEM_12("Cereal", "Cereal de avena, 300g", 45.00, 12),
     ITEM_13("Papel higiénico", "Paquete de 4 rollos", 35.00, 13),
     ITEM_14("Detergente", "Detergente líquido para ropa, 1 litro", 55.00, 14),
-    ITEM_15("Jabón", "Jabón de tocador, 3 barras", 20.00, 15);
+    ITEM_15("Jabón", "Jabón de tocador, 3 barras", 20.00, 15),
+    ITEM_16("Manteca", "Manteca vegetal, 500g", 40.00, 16),
+    ITEM_17("Yogurt", "Yogurt natural, 1 litro", 35.00, 17),
+    ITEM_18("Jugo", "Jugo de naranja, 1 litro", 25.00, 18),
+    ITEM_19("Papitas", "Papas fritas, 200g", 15.00, 19),
+    ITEM_20("Tortillas", "Tortillas de maíz, paquete de 1kg", 25.00, 20),
+    ITEM_21("Sopa", "Sopa instantánea, paquete de 5", 20.00, 21),
+    ITEM_22("Chiles", "Chiles frescos, 200g", 10.00, 22),
+    ITEM_23("Aguacate", "Aguacates frescos, 3 piezas", 50.00, 23),
+    ITEM_24("Pechuga de pollo", "Pechuga de pollo, 1kg", 80.00, 24),
+    ITEM_25("Pescado", "Pescado fresco, 1kg", 120.00, 25),
+    ITEM_26("Té", "Té verde, 20 bolsitas", 30.00, 26),
+    ITEM_27("Chocolates", "Caja de chocolates, 200g", 60.00, 27),
+    ITEM_28("Frijoles", "Frijoles negros, 1kg", 35.00, 28),
+    ITEM_29("Papas", "Papas frescas, 1kg", 25.00, 29),
+    ITEM_30("Cebolla", "Cebollas frescas, 1kg", 18.00, 30),
+    ITEM_31("Manzanas", "Manzanas rojas, 1kg", 40.00, 31),
+    ITEM_32("Plátanos", "Plátanos frescos, 1kg", 20.00, 32),
+    ITEM_33("Zanahorias", "Zanahorias frescas, 1kg", 18.00, 33),
+    ITEM_34("Pechuga de pavo", "Pechuga de pavo, 1kg", 100.00, 34),
+    ITEM_35("Chiles jalapeños", "Chiles jalapeños frescos, 200g", 15.00, 35);
 
     private final String nombre;
     private final String descripcion;
